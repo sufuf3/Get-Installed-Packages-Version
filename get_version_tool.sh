b7369b2a82fe9ea45c0f7a3a722beb8b1d771714
sufuf3 <sufuf3@gmail.com>
2018-04-05 16:07:19 +0800

    Add cuda, java, pip, pip3, jupyter version check

--- get_version_tool.sh
+++ get_version_tool.sh
@@ -14,27 +14,39 @@ echo "# Packages' Verson"
 echo "# Packages' Verson" >> versions-$date.md
 for p in $Packages
 do
-    echo "$p: `apt-cache policy $p | grep Installed | awk '{print $2}'`"
-    echo "$p: `apt-cache policy $p | grep Installed | awk '{print $2}'`" >> versions-$date.md
+    echo "$p==`apt-cache policy $p | grep Installed | awk '{print $2}'`"
+    echo "$p==`apt-cache policy $p | grep Installed | awk '{print $2}'`" >> versions-$date.md
 done
 
 ##### Java Version #####
 echo "# Java Version"
-echo "# Java Version"
-java -version
+try{
+    java -version
+    echo "# Java Version" >> versions-$date.md
+    java -version >> versions-$date.md
+}
+catch || {
+}
 
 ##### Python2 pip packages Version #####
 echo "# python2 packages version"
-echo "# python2 packages version"
+echo "# python2 packages version" >> versions-$date.md
+pip list --format=freeze
+pip list --format=freeze >> versions-$date.md
 
 ##### Python3 pip3 packages Version #####
 echo "# python3 packages version"
-echo "# python3 packages version"
+echo "# python3 packages version" >> versions-$date.md
+pip3 list --format=freeze
+pip3 list --format=freeze >> versions-$date.md
 
 ##### Cuda Version #####
 echo "# nvcc version"
-echo "# nvcc version"
+echo "# nvcc version" >> versions-$date.md
+nvcc --version >> versions-$date.md
 
 ##### Jupyter notebook Version #####
 echo "# jupyter notebook version"
-echo "# jupyter notebook version"
+echo "# jupyter notebook version" >> versions-$date.md
+jupyter notebook --version
+jupyter notebook --version >> versions-$date.md
